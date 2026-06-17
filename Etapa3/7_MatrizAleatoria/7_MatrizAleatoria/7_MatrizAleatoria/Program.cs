c104046de746ecfd907d12eac337a6a89cf20542
STabarcacheCano <samicamily09@gmail.com>
2026-06-17 14:30:07 -0300

    tp7

---
+++ Etapa3/7_MatrizAleatoria/7_MatrizAleatoria/7_MatrizAleatoria/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_MatrizAleatoria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random n = new Random();
+            int n_matriz = n.Next(1, 100);
+
+            Random m = new Random();
+            int m_matriz = m.Next(1, 100);
+
+            int[,] matriz = new int[n_matriz , m_matriz];
+
+            for (int i = 0; i < n_matriz; i++)
+            {
+                for (int j = 0; j < m_matriz; j++)
+                {
+                    Console.Write(matriz[i, j] + "-");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
