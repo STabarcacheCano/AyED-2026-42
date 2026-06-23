a6a19a48ee5086b9edaf0c38a4c8cd6850d82a8e
STabarcacheCano <samicamily09@gmail.com>
2026-06-23 13:51:04 -0300

    tp 9 MatrizRotada90

---
+++ Etapa3/9_MatrizRotada90/9_MatrizRotada90/9_MatrizRotada90/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_MatrizRotada90
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 5;
+            Random valor = new Random();
+
+            int[,] matriz = new int[n, n];
+            int[,] matriz90 = new int[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+
+                    int a = valor.Next(1, 10);
+                    matriz[i, j] = a;
+                    Console.Write(matriz[i, j] + "-");
+                }
+                Console.WriteLine("");
+            }
+            Console.WriteLine("");
+
+
+            for (int i = 0; i < n; i++)
+            {
+
+                for (int j = 0; j < n; j++)
+                {
+                    matriz90[j, (n - 1 - i)] = matriz[i, j];
+                }
+
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(matriz90[i, j] + "-");
+                }
+                Console.WriteLine("");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
