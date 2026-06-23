c69930a370754aba2d1e2e83a594545db6bbf30d
STabarcacheCano <samicamily09@gmail.com>
2026-06-23 13:53:38 -0300

    tp 10 diagonal

---
+++ Etapa3/10_ObtenerDiagonalPrincipal/10_DiagonalPrincipal/10_DiagonalPrincipal/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_DiagonalPrincipal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matriz = new int[5, 5];
+            int[] diagonal_principal = new int[5];
+
+            Random valor = new Random();
+
+            for( int i= 0; i<5; i++)
+            {
+                for (int j =0; j<5; j++)
+                {
+                    matriz[i,j]= valor.Next(1, 10);
+                }
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write(matriz[i, j] + " ");
+                }
+                Console.WriteLine("");
+            }
+
+            Console.WriteLine("");
+
+            for (int h = 0; h<5; h++)
+            {
+                diagonal_principal[h] = matriz[h, h];
+            }
+
+            for (int k = 0; k<5; k++)
+            {
+                Console.Write(diagonal_principal[k] + " ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Etapa3/10_ObtenerDiagonalPrincipal/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int[] numeros = new int[10];
+
+    
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"Ingrese el número: ");
+                numeros[i] = int.Parse(Console.ReadLine());
+            }
+
+            Array.Sort(numeros);
+
+            Console.WriteLine("Números ordenados:");
+            foreach (int num in numeros)
+            {
+                Console.WriteLine(num + " ");
+            }
+            
+            Console.WriteLine("Ingrese un número a buscar: ");
+            int buscar = int.Parse(Console.ReadLine());
+
+            bool encontrado = false;
+
+            foreach (int num in numeros)
+            {
+                if (num == buscar)
+                {
+                    encontrado = true;
+                    break;
+                }
+            }
+
+            if (encontrado)
+            {
+                Console.WriteLine("El número sí está en la lista.");
+            }
+            else
+            {
+                Console.WriteLine("no esta en la lista el numero ingresado");
+                if(encontrado = true)
+                {
+                    Console.WriteLine("Ingrese un número a buscar: ");
+                    int bus = int.Parse(Console.ReadLine());
+                    foreach (int num in numeros)
+                    {
+                        if (num == buscar)
+                        {
+                            encontrado = true;
+                            break;
+                        }
+                    }
+
+                    if (encontrado)
+                    {
+                        Console.WriteLine("El número sí está en la lista.");
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
