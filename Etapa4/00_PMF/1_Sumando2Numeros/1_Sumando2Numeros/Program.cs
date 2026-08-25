668e6d71b721c1d9357823d0c640b8d6dba7d562
STabarcacheCano <samicamily09@gmail.com>
2026-08-25 14:00:20 -0300

    Tp1

---
+++ Etapa4/00_PMF/1_Sumando2Numeros/1_Sumando2Numeros/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Sumando2Numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingresar el primer número a sumar: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingresar el segundo número a sumar: ");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nLa suma de ambos números da: " + Sumar(b, a));
+
+            Console.ReadKey();
+        }
+        static int Sumar(int n, int nn)
+        {
+            int resultado = n + nn;
+            return resultado;
+        }
+    }
+}
