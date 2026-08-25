4fd112ec6f397c62f4caca7b056a2f1f514dff73
STabarcacheCano <samicamily09@gmail.com>
2026-08-25 13:50:16 -0300

    Tp0

---
+++ Etapa4/00_PMF/0-HolaNombre/0-HolaNombre/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_HolaNombre
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese su nombre: ");
+            string nombre = Console.ReadLine();
+            Saludar(nombre);
+            Console.ReadKey();
+        }
+        static void Saludar(string nombre)
+        {
+            Console.WriteLine("Hola " + nombre);
+        }
+    }
+}
